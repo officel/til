3a775bf6f687b86f1858da886c231119a30c1563
Yoshitaka Nishimura <nishimura_yoshitaka_gn@griphone.co.jp>
2016-11-21 19:01:38 +0900

    FIX typo & code format

--- bin/today.php
+++ bin/today.php
@@ -1,16 +1,17 @@
 <?php
 
-function getWeather(){
+function getWeather()
+{
     // weather_hacks
     $url = "http://weather.livedoor.com/forecast/webservice/json/v1?city=130010";
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url); 
+    curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $json =  curl_exec($ch);
-    $result_array = json_decode($json,true);
+    $json = curl_exec($ch);
+    $result_array = json_decode($json, true);
 
     $weather = [];
-    foreach($result_array['forecasts'] as $list){
+    foreach ($result_array['forecasts'] as $list) {
         $weather[] = $list['dateLabel'] . '（' . $list['telop'] . '）';
     }
 
@@ -20,17 +21,18 @@ function getWeather(){
     return $ret;
 }
 
-function getWikiRandom(){
-    // wikipeida api 
+function getWikiRandom()
+{
+    // wikipedia api
     $url = "https://ja.wikipedia.org/w/api.php?format=json&action=query&list=random&rnnamespace=0&rnlimit=3";
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url); 
+    curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $json =  curl_exec($ch);
-    $result_array = json_decode($json,true);
+    $json = curl_exec($ch);
+    $result_array = json_decode($json, true);
 
     $random = [];
-    foreach($result_array['query']['random'] as $list){
+    foreach ($result_array['query']['random'] as $list) {
         $random[] = '* [' . $list['title'] . '](https://ja.wikipedia.org/wiki/' . urlencode($list['title']) . ')';
     }
 
